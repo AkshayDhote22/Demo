871c76adf0c4005fda15075edd6a566746a556b9
Akshay <Akshay@HP>
2021-06-24 05:35:03 -0700

    List object added

--- Guit/src/Demo/Collection.java
+++ Guit/src/Demo/Collection.java
@@ -1,5 +1,11 @@
 package Demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collection {
+	public static void main(String[] args) {
+		List<String> l1= new ArrayList<String>();
+	}
 
 }
